d6c1a76c842a7eec370b47f8e8bba318bcfec4ea
N.Ugochukwu <115807244+Nugochukwu@users.noreply.github.com>
2023-05-14 17:33:10 +0100

    commit-12

---
+++ week-1/Project_2.py
@@ -0,0 +1,7 @@
+name1 = str(input("What is the first name "))
+age1 = int(input("What is the first age "))
+name2 = str(input("What is the second name "))
+age2 = int(input("What is the second age "))
+
+print(f"First name and swapped age = {name1} {age2}")
+print(f"Second name and swapped age = {name2} {age1}")
